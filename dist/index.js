4315f0b99702ac8e552ec66b8291ec6526e5a487
l2700l <thetypgame@gmail.com>
2023-12-17 07:19:50 +0400

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,54 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const enquirer_1 = __importDefault(require("enquirer"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const simple_git_1 = __importDefault(require("simple-git"));
+const git = (0, simple_git_1.default)();
+const init = () => __awaiter(void 0, void 0, void 0, function* () {
+    const { appname } = yield enquirer_1.default.prompt({
+        type: 'input',
+        name: 'appname',
+        message: 'Application app name?',
+    });
+    const templateRepoUrl = 'https://github.com/l2700l/vkma-ts-template';
+    const currentDirectory = process.cwd();
+    const destinationPath = path_1.default.join(currentDirectory, appname !== null && appname !== void 0 ? appname : 'vkma');
+    try {
+        yield downloadTemplate(templateRepoUrl, destinationPath);
+        instruction(appname !== null && appname !== void 0 ? appname : 'vkma');
+    }
+    catch (error) {
+        console.error('Error downloading template:', error);
+    }
+});
+const downloadTemplate = (templateUrl, destinationPath) => __awaiter(void 0, void 0, void 0, function* () {
+    yield git.clone(templateUrl, destinationPath, ['--depth=1']);
+    const gitFolderPath = path_1.default.join(destinationPath, '.git');
+    if (fs_1.default.existsSync(gitFolderPath)) {
+        fs_1.default.rmSync(gitFolderPath, { recursive: true });
+    }
+});
+const instruction = (appname) => {
+    console.log('Template downloaded successfully!');
+    console.log();
+    console.log('Next steps:');
+    console.log('1. cd', appname);
+    console.log('2. npm install');
+    console.log('3. npm run dev');
+    console.log();
+    console.log('Happy coding!');
+};
+init();
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import enquirer from 'enquirer';
+import path from 'path';
+import fs from 'fs';
+import simpleGit, { SimpleGit } from 'simple-git';
+
+const git: SimpleGit = simpleGit();
+
+const init = async () => {
+  const { appname } = await enquirer.prompt<{ appname: string }>({
+    type: 'input',
+    name: 'appname',
+    message: 'Application app name?',
+  });
+  const templateRepoUrl = 'https://github.com/l2700l/vkma-ts-template';
+  const currentDirectory = process.cwd();
+  const destinationPath = path.join(currentDirectory, appname ?? 'vkma');
+  try {
+    await downloadTemplate(templateRepoUrl, destinationPath);
+    instruction(appname ?? 'vkma');
+  } catch (error) {
+    console.error('Error downloading template:', error);
+  }
+};
+
+const downloadTemplate = async (
+  templateUrl: string,
+  destinationPath: string
+) => {
+  await git.clone(templateUrl, destinationPath, ['--depth=1']);
+  const gitFolderPath = path.join(destinationPath, '.git');
+  if (fs.existsSync(gitFolderPath)) {
+    fs.rmSync(gitFolderPath, { recursive: true });
+  }
+};
+
+const instruction = (appname: string) => {
+  console.log('Template downloaded successfully!');
+  console.log();
+  console.log('Next steps:');
+  console.log('1. cd', appname);
+  console.log('2. npm install');
+  console.log('3. npm run dev');
+  console.log();
+  console.log('Happy coding!');
+};
+
+init();
